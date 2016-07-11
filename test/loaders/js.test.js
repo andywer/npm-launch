2b78eea856124c1e9dfa5f4929b9d31ce284a45e
Andy Wermke <andy@dev.next-step-software.com>
2016-07-11 23:35:44 +0200

    Implement first basic tests for JS loader

---
+++ test/loaders/js.test.js
@@ -0,0 +1,63 @@
+const test = require('ava').test
+const path = require('path')
+const rewire = require('rewire')
+const EventEmitter = require('events')
+
+const taskClasses = require('../../lib/task/classes')
+
+const FIXTURE_FILE_PATH = path.join(__dirname, '../fixtures/launch.scripts.js')
+
+function take (thing, callback) {
+  callback(thing)
+}
+
+test('loading JS file', t => {
+  const jsLoader = require('../../lib/loaders/js')
+
+  const taskCallEmitter = new EventEmitter()
+  const tasks = jsLoader.loadFile(FIXTURE_FILE_PATH, taskCallEmitter)
+
+  t.deepEqual(
+    Object.keys(tasks).sort(),
+    [ 'default', 'fancyTasking', 'npmList', 'runStandardLinter', 'wasteSomeTime', 'willFail' ]
+  )
+
+  ///////////////////
+  // Task "default":
+  t.true(tasks.default instanceof taskClasses.TaskFork)
+  t.is(tasks.default.name, 'default')
+  t.is(tasks.default.children.length, 3)
+
+  take(tasks.default.children[0], (subTask) => {
+    t.true(subTask instanceof taskClasses.AnonymousCommandTask)
+    t.is(subTask.title, 'npmList')
+  })
+
+  ////////////////////////
+  // Task "fancyTasking":
+  t.true(tasks.fancyTasking instanceof taskClasses.TaskLeaf)
+  t.is(tasks.fancyTasking.name, 'fancyTasking')
+
+  ///////////////////
+  // Task "npmList":
+  t.true(tasks.npmList instanceof taskClasses.TaskLeaf)
+  t.is(tasks.npmList.name, 'npmList')
+
+  /////////////////////////////
+  // Task "runStandardLinter":
+  t.true(tasks.runStandardLinter instanceof taskClasses.TaskLeaf)
+  t.is(tasks.runStandardLinter.name, 'runStandardLinter')
+
+  /////////////////////////
+  // Task "wasteSomeTime":
+  t.true(tasks.wasteSomeTime instanceof taskClasses.TaskLeaf)
+  t.is(tasks.wasteSomeTime.name, 'wasteSomeTime')
+
+  ////////////////////
+  // Task "willFail":
+  t.true(tasks.willFail instanceof taskClasses.TaskLeaf)
+  t.is(tasks.willFail.name, 'willFail')
+})
+
+// TODO: Test if taskCallEmitter works (test each function [not task]; rewire `shell()`)
+// TODO: Test if Listr items are created as expected (test each task; rewire `shell()`)
